31f9bc891f8c52ca5de6c68ab0670225e21864f1
Md. Mahadhi Hassan Chowdhury <mhcsunny@gmail.com>
2021-11-08 16:13:23 +0600

    Initial Commit

---
+++ Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Workspace Infotech on 8/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
